071d7ce59fc91e2aea1e65b0d9d449fe74bc2ee4
billylinder <billy@billylinder.com>
2017-10-11 15:24:13 -0400

    Add products.php

---
+++ products.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "wi133961";
+$password = "Mackenzie12!";
+$dbname = "wi133961";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["name"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
